8a99db24ab9b8fb697dd98c8168ef806b18bfd5a
victor.bautista <victor.bautista@dd51d12a-d5b1-11dd-a297-4fb579711a53>
2008-12-29 16:46:50 +0000

    added delicious share button

--- trunk/reader.js
+++ trunk/reader.js
@@ -1,4 +1,4 @@
-var fbreader={version:"0.1",
+var fbreader={version:"0.2",
     addScripts:function(){
         if(typeof jQuery=='function')
             return;
@@ -14,23 +14,33 @@ var fbreader={version:"0.1",
         jQuery.noConflict();
 
         if(jQuery("#current-entry").length>0)
-            fbreader.addfacebook();
-            jQuery(".entry").bind("click",fbreader.addfacebook)
-        }, addfacebook:function(){
-
+            fbreader.addnewitems();
+            jQuery(".entry").bind("click",fbreader.addnewitems)
+        }, addnewitems:function(){
             var b=jQuery("#current-entry");
             var c=jQuery(".entry-actions",b);
             if(jQuery("#facebook").length!=0)
                 jQuery("#facebook").remove();
-            jQuery(c).append('<span id="facebook" class="comment link unselectable"><span class="entry-comment-action-title">FaceBook</span></span>');
+            jQuery(c).append('<span id="facebook" class="link unselectable"><span class="entry-share-action-facebook">FaceBook</span></span>');
             jQuery("#facebook").css({
                 background:"url(http://fbreader.googlecode.com/files/facebook_icon.png) no-repeat",paddingLeft:"14px"
             });
 
-            jQuery(".entry-comment-action-title").bind("click",function(){
+            jQuery(c).append('<span id="delicious" class="link unselectable"><span class="entry-share-action-delicious">Delicious</span></span>');
+            jQuery("#delicious").css({
+                background:"url(http://fbreader.googlecode.com/files/facebook_icon.png) no-repeat",paddingLeft:"14px"
+            });
+
+            jQuery(".entry-comment-action-facebook").bind("click",function(){
                 var u=jQuery("a.entry-title-link",jQuery("#current-entry")).attr("href");
                 window.open('http://www.facebook.com/share.php?u='+u,'_blank');
+            });
+            jQuery(".entry-comment-action-delicious").bind("click",function(){
+                var u=jQuery("a.entry-title-link",jQuery("#current-entry")).attr("href");
+                var t=jQuery("a.entry-title-link",jQuery("#current-entry")).text();
+                window.open('http://del.icio.us/post?url='+u+'&title='+t, '_blank');
             })
+
         },init:function(){
             if(typeof jQuery!='function')
                 this.addScripts();
