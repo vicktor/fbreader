316d09a0dc2e2dc6ab93bf17ee17f256389c7bf6
victor.bautista <victor.bautista@dd51d12a-d5b1-11dd-a297-4fb579711a53>
2009-01-09 15:24:47 +0000

    Added meneame, thanks to anieto2k

--- trunk/reader.js
+++ trunk/reader.js
@@ -1,3 +1,22 @@
+var items = [
+	{ 
+		name: "Facebook", 
+		slink: "http://www.facebook.com/share.php?u=%link%",
+		ico: "http://fbreader.googlecode.com/files/facebook_icon.png"
+	},
+	{
+		name: "Del.icio.us", 
+		slink: "http://del.icio.us/post?url=%link%&title=%title%",
+		ico: "http://fbreader.googlecode.com/files/delicious_icon.png"
+	},
+	{
+		name: "Meneame", 
+		slink: "http://meneame.net/submit.php?url=%link%",
+		ico: "http://meneame.net/favicon.ico"
+	}
+	];
+
+
 var fbreader={version:"0.2",
     addScripts:function(){
         if(typeof jQuery=='function')
@@ -8,13 +27,33 @@ var fbreader={version:"0.2",
         s.setAttribute("id","script_fbreader");
         s.onload=s.onreadystatechange=this.cargando;
         document.getElementsByTagName("head")[0].appendChild(s)
-    },cargando:function(){
+    },
+    cargando:function(){
         if(typeof jQuery!='function')
             return;
         jQuery.noConflict();
 
         if(jQuery("#current-entry").length>0)
             fbreader.addnewitems();
+<<<<<<< .mine
+        jQuery(".entry").bind("click",fbreader.addnewitems);
+		}, 
+    addnewitems:function(){
+        var b=jQuery("#current-entry");
+        var c=jQuery(".entry-actions",b);
+        
+        for (var x in items) {
+        	var name = items[x].name;
+					console.log("Name: " + name);
+					
+        	// if(jQuery("#" + name).length!=0) continue;
+        	
+        	var slink = items[x].slink;
+        	var ico = items[x].ico;
+        	
+        	jQuery(c).append('<span id="'+name+'" class="broadcast link unselectable"><span class="entry-share-action-'+name+'">'+name+'</span></span>');
+        	jQuery("#" + name).css({background:"url("+ico+") no-repeat",paddingLeft:"14px"});
+=======
             jQuery(".entry").bind("click",fbreader.addnewitems)
         }, addnewitems:function(){
             var b=jQuery("#current-entry");
@@ -27,7 +66,16 @@ var fbreader={version:"0.2",
             jQuery("#facebook").css({
                 background:"url(http://fbreader.googlecode.com/files/facebook_icon.png) no-repeat",paddingLeft:"14px"
             });
+>>>>>>> .r11
 
+<<<<<<< .mine
+          jQuery(".entry-share-action-"+name).bind("click",function(){
+              var u=jQuery("a.entry-title-link",jQuery("#current-entry")).attr("href");
+              var t=jQuery("a.entry-title-link",jQuery("#current-entry")).text();
+              var shareUrl = slink.replace("%link%", escape(u)).replace("%title%", escape(t));
+              window.open(shareUrl, '_blank');
+          });
+=======
             jQuery(c).append('<span id="delicious" class="broadcast link unselectable"><span class="entry-share-action-delicious">Delicious</span></span>');
             jQuery("#delicious").css({
                 background:"url(http://fbreader.googlecode.com/files/delicious_icon.png) no-repeat",paddingLeft:"14px"
@@ -47,7 +95,14 @@ var fbreader={version:"0.2",
             if(typeof jQuery!='function')
                 this.addScripts();
             else this.cargando()
+>>>>>>> .r11
         }
-    };
+    },
+    init:function(){
+        if(typeof jQuery!='function')
+            this.addScripts();
+        else this.cargando()
+    }
+};
 fbreader.init();
 
